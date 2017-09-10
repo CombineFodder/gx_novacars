3a52682b195ad722ace99015164f215d4ca32ce6
Revolver Ocelot <cmb.fodder@gmail.com>
2017-09-10 13:18:37 +0700

    Create gx_othercars.lua

---
+++ gx_othercars.lua
@@ -0,0 +1,88 @@
+CMOD_Cars = CMOD_Cars or {}
+CMOD_Cars_AUX = CMOD_Cars_AUX or {}
+local t = CMOD_Cars_AUX	
+  t["uaz315195gx"] = {
+		["Name"] = "UAZ-315195",
+		["Passengers"] = {
+			[1] = { ["x"] = 18, ["y"] = -6, ["z"] = 40},
+			[2] = { ["x"] = -18, ["y"] = 34, ["z"] = 40},
+			[3] = { ["x"] = 18, ["y"] = 34, ["z"] = 40},
+			[4] = { ["x"] = 0, ["y"] = 34, ["z"] = 40},
+		},
+		["DamageResist"] = 1,
+		["Headlights"] = {
+			[1] = Vector( 28.11, 109.75, 52.83 ),
+			[2] = Vector( -28.11, 109.75, 52.83 ),
+		},
+		["TailLights"] = { Vector( -35.11, -79.75, 41.83 ), Vector( 35.11, -79.75, 41.83 ) },
+		["EnginePos"] = Vector( 0, 78.55, 59.43 ),
+		["WheelHeightFix"] = 0,
+	}
+	
+	t["uaz2206pgx"] = {
+		["Name"] = "UAZ 2206 Police",
+		["Passengers"] = {
+			[1] = { ["x"] = 27, ["y"] = -40, ["z"] = 27},
+			[2] = { ["x"] = -27, ["y"] = 28, ["z"] = 20},
+			[3] = { ["x"] = 27, ["y"] = 28, ["z"] = 20},
+			[4] = { ["x"] = 0, ["y"] = 28, ["z"] = 20},
+		},
+		["DamageResist"] = 1,
+		["Headlights"] = {
+			[1] = Vector( -24.52, 86.68, 36.56 ),
+			[2] = Vector( 24.52, 86.68, 36.56 ),
+		},
+		["TailLights"] = { Vector( -40.52, -111.68, 30.56 ), Vector( 40.52, -111.68, 30.56 ) },
+		["EnginePos"] = Vector( 0, 70.55, 20.43 ),
+		["WheelHeightFix"] = 0,
+	}
+	
+	t["zilgorsvetgx"] = {
+		["Name"] = "ZIL 130 GORSVET",
+		["Passengers"] = {
+			[1] = { ["x"] = 27, ["y"] = -59, ["z"] = 70.33},
+			[2] = { ["x"] = 7, ["y"] = -59, ["z"] = 70.33},
+		},
+		["DamageResist"] = 1,
+		["Headlights"] = {
+			[1] = Vector( 42.27, 165.87, 73.62 ),
+			[2] = Vector( -40.27, 165.87, 73.62 ),
+		},
+		["TailLights"] = { Vector( 42.27, -165.87, 63.62 ), Vector( -40.27, -165.87, 63.62 ) },
+		["EnginePos"] = Vector( 0, 140.55, 80.43 ),
+		["WheelHeightFix"] = 0,
+	}
+  
+		t["izh27151gx"] = {
+		["Name"] = "IZH-27151",
+		["Passengers"] = {
+			[1] = { ["x"] = 17, ["y"] = -7, ["z"] = 7},
+		},
+		["DamageResist"] = 1,
+		["Headlights"] = {
+			[1] = Vector( 24.19, 98.89, 22.1 ),
+			[2] = Vector( -24.19, 98.89, 22.1 ),
+		},
+		["TailLights"] = { Vector( 23.19, -92.89, 13.1 ), Vector( -23.19, -92.89, 13.1 ) },
+		["EnginePos"] = Vector( 0, 78.55, 39.43 ),
+		["WheelHeightFix"] = 0,
+	}
+  
+  	t["azlk2140gx"] = {
+		["Name"] = "AZLK 2140",
+		["Passengers"] = {
+			[1] = { ["x"] = 18, ["y"] = -5, ["z"] = 10},
+			[2] = { ["x"] = -18, ["y"] = 35, ["z"] = 10},
+			[3] = { ["x"] = 18, ["y"] = 35, ["z"] = 10},
+			[4] = { ["x"] = 0, ["y"] = 35, ["z"] = 10},
+		},
+		["DamageResist"] = 1,
+		["Headlights"] = {
+			[1] = Vector( 26.15, 91.84, 25.26 ),
+			[2] = Vector( -26.15, 91.84, 25.26 ),
+		},
+		["TailLights"] = { Vector( 22.15, -95.84, 25.26 ), Vector( -22.15, -95.84, 25.26 ) },
+		["EnginePos"] = Vector( 0, 92.55, 39.43 ),
+		["WheelHeightFix"] = 0,
+	}
+}
