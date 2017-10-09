d278efb56666c4bc1687c4d3d3d1e35b6ed1d8a0
Revolver Ocelot <cmb.fodder@gmail.com>
2017-10-09 17:40:20 +0700

    Update gx_toyota.lua

--- gx_toyota.lua
+++ gx_toyota.lua
@@ -15,3 +15,40 @@
 		["TailLights"] = { Vector( 28.91, -105.27, 27.92 ), Vector( -28.91, -105.27, 27.92 ) },
 		["WheelHeightFix"] = 0,
 	}
+	t["levingx"] = {
+		["Name"] = "Toyota Levin HachiGo",
+		["Passengers"] = {
+			[1] = { x = -18, y = -3.36, z = 6.69},
+			[2] = { x = 18, y = 36.64, z = 6.69},
+			[3] = { x = -18, y = 36.64, z = 6.69},
+			[3] = { x = 0, y = 36.64, z = 6.69},
+		},
+		["DamageResist"] = 1,
+		["Headlights"] = {
+			[1] = Vector( 26.25, 105.71, 24.88 ),
+			[2] = Vector( -26.25, 105.71, 24.88 ),
+		},
+		["EnginePos"] = Vector( 0, 72.83, 39.86 ),
+		["TailLights"] = { Vector( -27.07, -99.38, 28.39 ), Vector( 27.07, -99.38, 28.39 ) },
+		["WheelHeightFix"] = 0,
+	}
+
+	t["altezzagx"] = {
+		["Name"] = "Toyota Altezza RS200",
+		["Passengers"] = {
+			[1] = { x = -18, y = 2.09, z = 9.49},
+			[2] = { x = 18, y = 43.59, z = 9.49},
+			[3] = { x = -18, y = 43.59, z = 9.49},
+			[4] = { x = 0, y = 43.59, z = 9.49},
+		},
+		["DamageResist"] = 1,
+		["Headlights"] = {
+			[1] = Vector( 35.2, 102.11, 26.84 ),
+			[2] = Vector( -35.2, 102.11, 26.84 ),
+			[3] = Vector( 27.68, 104.85, 26.07 ),
+			[4] = Vector( -27.68, 104.85, 26.07 ),
+		},
+		["EnginePos"] = Vector( 0, 92.55, 39.43 ),
+		["TailLights"] = { Vector( -21.51, -123.39, 38.26 ), Vector( 21.51, -123.39, 38.26 ), Vector( -32.98, -120.39, 38.26 ), Vector( 32.98, -120.39, 38.26 ) },
+		["WheelHeightFix"] = 0,
+	}
