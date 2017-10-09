bdcf3dc83bd67b85347760c5ea17628b722ff0b3
Revolver Ocelot <cmb.fodder@gmail.com>
2017-10-09 17:18:48 +0700

    Create gx_skyline.lua

---
+++ gx_skyline.lua
@@ -0,0 +1,38 @@
+	t["skyline_r32gx"] = {
+		["Name"] = "Nissan Skyline R32",
+		["Passengers"] = {
+			[1] = { x = -18, y = 0, z = 1.75},
+			[2] = { x = -18, y = 40, z = 1.75},
+			[3] = { x = 18, y = 40, z = 1.75},
+			[4] = { x = 0, y = 40, z = 1.75},
+		},
+		["DamageResist"] = 1,
+		["Headlights"] = {
+			[1] = Vector( -26.5, 106.67, 21.66 ),
+			[2] = Vector( 26.5, 106.67, 21.66 ),
+		},
+		["TailLights"] = { Vector( -21.19, -103.06, 28.44 ), Vector( 21.19, -103.06, 28.44 ), Vector( 32.61, -103, 28.03 ), Vector( -32.61, -103, 28.03 ) },
+		["EnginePos"] = Vector( 0, 81.82, 32.07 ),
+		["WheelHeightFix"] = 0,
+	}
+
+	t["skyline_r34gx"] = {
+		["Name"] = "Nissan Skyline R34",
+		["Passengers"] = {
+			[1] = { x = -18, y = -4.05, z = 4.09},
+			[2] = { x = -18, y = 35.95, z = 4.09},
+			[3] = { x = 18, y = 35.95, z = 4.09},
+			[4] = { x = 0, y = 35.95, z = 4.09},
+		},
+		["DamageResist"] = 1,
+		["Headlights"] = {
+			[1] = Vector( -27.94, 102.93, 22.4 ),
+			[2] = Vector( 27.94, 102.93, 22.4 ),
+			[3] = Vector( -19.85, 102.29, 22.4 ),
+			[4] = Vector( 19.85, 102.29, 22.4 ),
+
+		},
+		["TailLights"] = { Vector( 30.45, -87.88, 30.58  ), Vector( -30.45, -87.88, 30.58  ) },
+		["EnginePos"] = Vector( 0, 81.82, 32.07 ),
+		["WheelHeightFix"] = 0,
+	}
